cc97b6073bd4dc7194e9e21d276b0c6d33192d17
Thais Nishimoto <thaismnishimoto@gmail.com>
2023-06-14 12:51:52 -0300

    implementing function

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,15 +6,26 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 15:11:48 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/06/13 15:37:29 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/06/14 12:50:55 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *str, ...)
 {
 	va_list	args;
-	va_list(args)
+	int	i;
+	int	len;
 
+	i = 0;
+	len = 0;
+	while (str[i])
+	{
+		if (str[i] != '%')
+			len++;
+		i++;
+	}
+	ft_strlen(str)
+	return (0);
 }
